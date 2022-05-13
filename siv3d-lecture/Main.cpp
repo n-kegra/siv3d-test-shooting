6197e5f39a9fad3ea4c6e6ebe22dac280e4e01cf
kegra <nukegarara@yahoo.co.jp>
2022-05-13 13:51:09 +0900

    ショットを追加

--- siv3d-lecture/Main.cpp
+++ siv3d-lecture/Main.cpp
@@ -4,6 +4,8 @@ void Main()
 {
 	Vec2 p = { 100, 100 };
 
+	Array<Vec2> shots;
+
 	while (System::Update())
 	{
 		if (KeyUp.pressed()) {
@@ -17,6 +19,14 @@ void Main()
 		}
 		if (KeyRight.pressed()) {
 			p.x += 10;
+		}
+
+		if (KeySpace.down()) {
+			shots.push_back(p);
+		}
+
+		for (const auto& shot : shots) {
+			Circle(shot, 5).draw(Palette::Green);
 		}
 
 		RectF(Arg::center(p), Size(30, 30)).draw(Palette::Blue);
