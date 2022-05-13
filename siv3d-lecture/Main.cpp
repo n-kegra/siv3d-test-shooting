32d2fbb9a0c4e12b05614e59c96b1e8dd4cc2f2d
kegra <nukegarara@yahoo.co.jp>
2022-05-13 13:41:04 +0900

    スペースキーで表示するようにしてみた

--- siv3d-lecture/Main.cpp
+++ siv3d-lecture/Main.cpp
@@ -4,6 +4,8 @@ void Main()
 {
 	while (System::Update())
 	{
-		RectF(Vec2(100, 100), Size(30, 30)).draw(Palette::Blue);
+		if (KeySpace.pressed()) {
+			RectF(Vec2(100, 100), Size(30, 30)).draw(Palette::Blue);
+		}
 	}
 }
